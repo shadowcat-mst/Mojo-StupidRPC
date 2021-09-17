de0476dd7279daaa8877c43d96a21eae63594e51
Matt S Trout <mst@shadowcat.co.uk>
2021-09-17 00:22:13 +0000

    add panic avoidance code, no I dunno why, must investigate later

--- lib/Mojo/StupidRPC/BecomesActive.pm
+++ lib/Mojo/StupidRPC/BecomesActive.pm
@@ -6,7 +6,10 @@ has active => 0;
 
 sub name ($self) { $self->tag =~ /:(.*)$/ }
 
-after done => sub ($self, @) {
+# the next three lines are because I was getting perl panics; must investigate
+# after done => sub ($self, @) {
+around done => sub ($orig, $self, @args) {
+  $self->$orig(@args);
   $self->tag(join ':', $self->type, $self->name);
   $self->_register;
   $self->active(1);
